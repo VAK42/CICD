5a64208de5d874f55b6d27e3f2d861cac1b566d7
Vũ Anh Kiệt <vutrandangkhoa7@gmail.com>
2026-08-25 16:32:05 +0700

    .

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 export const metadata: Metadata = {
   title: "VAK",
-  description: "OK"
+  description: "CICD"
 };
 export default function RootLayout({
   children
